1081c5dcb547395efa946bee5f1e88ecd5987392
Aurelio <aurelio.sg3@outlook.com>
2024-06-05 09:20:21 -0300

    login and logout routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,25 +5,30 @@ import { getUserByIdController } from '../useCases/Users/GetUserById'
 import { createUserController } from '../useCases/Users/CreateUser'
 import { deleteUserByIdController } from '../useCases/Users/DeleteUserById'
 import { updateUserByIdController } from '../useCases/Users/UpdateUserById'
-const router = Router()
+import { loginUserController } from '../useCases/Users/LoginUser'
+import { logoutUserController } from '../useCases/Users/LogoutUser'
+const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-    return getUsersController.handle(req, res)    
+    return getUsersController.handle(req, res)
 })
 router.get("/:user_id", (req: Request, res: Response) => {
-    return getUserByIdController.handle(req, res)    
+    return getUserByIdController.handle(req, res)
 })
 router.post("/", (req: Request, res: Response) => {
     return createUserController.handle(req, res)
 })
+router.post("/login", (req: Request, res: Response) => {
+    return loginUserController.handle(req, res)
+})
 router.patch("/:user_id", (req: Request, res: Response) => {
-    return updateUserByIdController.handle(req, res)    
+    return updateUserByIdController.handle(req, res)
 })
 router.delete("/:user_id", (req: Request, res: Response) => {
-    return deleteUserByIdController.handle(req, res)    
+    return deleteUserByIdController.handle(req, res)
 })
-// router.post("/")
-// router.put("/:id")
-// router.delete("/:id")
+router.delete("/logout", (req: Request, res: Response) => {
+    return logoutUserController.handle(req, res)
+})
 
 export default router;
